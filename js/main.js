3440b6ecf3232c509ad27779abed5978d386bd74
Somedef <osh0082@naver.com>
2022-11-03 21:21:14 +0900

    고정헤더수정

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
+AOS.init();
 
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 400) {
+        $('.go-top').fadeIn();
+    } else {
+        $('.go-top').fadeOut();
+    }
+});
+$('.go-top').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 400);
+    return false;
+});
+// 탑버튼
+
+$(window).on('scroll', function () {
+    if ($(window).scrollTop()) {
+        $('header').addClass('active');
+    } else {
+        $('header').removeClass('active');
+    }
+});
+            // 헤더
 
 
 
